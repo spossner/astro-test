737cdebc33d8dc92cbee21c80bc03c88414d3087
spossner <github@possner.de>
2023-02-16 07:56:14 +0100

    feature: add responsive navigation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "#4c1d95",
+				secondary: "#ff9776",
+			}
+		},
 	},
 	plugins: [],
 }
